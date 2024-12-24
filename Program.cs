e272ba4ee97443542a298dd31a0215b6faa30008
nira10 <nirafogel@gmail.com>
2024-12-24 09:12:07 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HtmlProcessor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HtmlSerializer loader = new HtmlSerializer();
+            string url = "https://www.example.com";
+
+            string htmlContent = await loader.Load(url);
+
+            Console.WriteLine(htmlContent);
+        }
+
+    }
+}
+
